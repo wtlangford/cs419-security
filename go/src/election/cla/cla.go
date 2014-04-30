756935e8f744c0eece56b6b785bce5a9822df139
Billy Lynch <wlynch92@gmail.com>
2014-04-30 00:27:12 -0400

    Added basic form handling for CLA

--- go/src/election/cla/cla.go
+++ go/src/election/cla/cla.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"log"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
 	"net/http"
 )
 
+var db map[string]string = map[string]string {
+	"a": "1",
+	"b": "2",
+}
+
 // Generic handler for a generic page.
-func handler(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Content-Type", "text/plain")
-	w.Write([]byte("This is an example server.\n"))
+func handler() string {
+	return "hello world"
+}
+
+type Registration struct {
+	Name string `form:"name"`
+	Secret string `form:"secret"`
+}
+
+func RegisterUser(reg Registration) string{
+	log.Println(reg)
+	return "asdf"
 }
 
 func main() {
-	http.HandleFunc("/", handler)
-	log.Printf("About to listen on 1443. Go to https://127.0.0.1:1443/")
-	if err := http.ListenAndServeTLS(":1444", "cert.pem", "key.pem", nil); err != nil {
-		log.Fatal(err)
-	}
+	m := martini.Classic()
+	m.Post("/register", binding.Bind(Registration{}), RegisterUser)
+	m.Get("/", handler)
+	log.Println("About to listen on 1444. Go to https://localhost:1444/")
+	log.Fatal(http.ListenAndServeTLS(":1444", "cert.pem", "key.pem", m))
 }
