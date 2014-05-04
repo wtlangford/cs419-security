c93e5cca0995188706d0d96fa6f7152d7e211067
Billy Lynch <wlynch92@gmail.com>
2014-05-04 15:12:20 -0400

    Added keys, gofmt

--- go/src/election/ctf/ctf.go
+++ go/src/election/ctf/ctf.go
@@ -8,43 +8,41 @@ import (
 	"encoding/base64"
 	"encoding/pem"
 	"fmt"
+	"github.com/go-martini/martini"
+	"github.com/martini-contrib/binding"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"github.com/go-martini/martini"
-	"github.com/martini-contrib/binding"
 	"sync"
 )
 
-type CTF struct{
+type CTF struct {
 	sync.RWMutex
 	validationNumbers map[string]bool
-	votes map[string] []string
+	votes             map[string][]string
 }
 
 type FormPost struct {
 	ValNum string `form:"vn"`
-	Sig string `form:"sig"`
-	Id string `form:"id"`
-	Vote string `form:"vote"`
+	Sig    string `form:"sig"`
+	Id     string `form:"id"`
+	Vote   string `form:"vote"`
 }
 
-
-var ctf CTF = CTF{validationNumbers:make(map[string]bool), votes:make(map[string] []string)}
-var choices []string = []string{"tacocat","racecar","radar","civic"}
+var ctf CTF = CTF{validationNumbers: make(map[string]bool), votes: make(map[string][]string)}
+var choices []string = []string{"tacocat", "racecar", "radar", "civic"}
 var claKey *rsa.PublicKey
 
 func stringInSlice(a string, list []string) bool {
-    for _, b := range list {
-        if b == a {
-            return true
-        }
-    }
-    return false
+	for _, b := range list {
+		if b == a {
+			return true
+		}
+	}
+	return false
 }
 
-
-func VerifySig (message []byte, sig []byte, r *rsa.PublicKey) error {
+func VerifySig(message []byte, sig []byte, r *rsa.PublicKey) error {
 	h := sha512.New()
 	h.Write(message)
 	d := h.Sum(nil)
@@ -79,18 +77,18 @@ func vote(params FormPost) string {
 	vote := params.Vote
 
 	ctf.Lock()
-	if v,ok := ctf.validationNumbers[vn]; ok == false {
+	if v, ok := ctf.validationNumbers[vn]; ok == false {
 		ctf.Unlock()
-		return fmt.Sprint("This vn does not exist...",vn,"x",params,"\n")
-	} else if  v == false {
+		return fmt.Sprint("This vn does not exist...", vn, "x", params, "\n")
+	} else if v == false {
 		ctf.Unlock()
 		return "This vn has already voted..."
-	} else if !stringInSlice(vote,choices) {
+	} else if !stringInSlice(vote, choices) {
 		ctf.Unlock()
 		return "Invalid vote"
 	}
 
-	ctf.votes[vote] = append(ctf.votes[vote],id)
+	ctf.votes[vote] = append(ctf.votes[vote], id)
 	ctf.validationNumbers[vn] = false
 	res := fmt.Sprint(ctf.votes[vote])
 	ctf.Unlock()
@@ -111,23 +109,23 @@ func main() {
 		log.Fatal("Could not parse CLA Public Key")
 	}
 	switch t := pubkey.(type) {
-		case *rsa.PublicKey:
-			claKey = t
-		default:
-			log.Fatal("unknown key type")
+	case *rsa.PublicKey:
+		claKey = t
+	default:
+		log.Fatal("unknown key type")
 	}
 
 	m := martini.Classic()
-	m.Post("/vn",binding.Bind(FormPost{}),addValidationNumber)
-	m.Post("/vote",binding.Bind(FormPost{}),vote)
-	m.Get("/results",func() string {
+	m.Post("/vn", binding.Bind(FormPost{}), addValidationNumber)
+	m.Post("/vote", binding.Bind(FormPost{}), vote)
+	m.Get("/results", func() string {
 		ctf.RLock()
 		str := fmt.Sprint(ctf.votes)
 		ctf.RUnlock()
 		return str
 	})
 
-	m.Get("/",func() string {
+	m.Get("/", func() string {
 		return "Martini up!"
 	})
 	http.ListenAndServeTLS(":4000", "cert.pem", "key.pem", m)
